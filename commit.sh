162cb33f6ed1894392ea6d67ceb7572602789c4c
Isac Canedo <32867124+isaccanedo@users.noreply.github.com>
2024-04-21 12:27:25 -0300

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+# Obter data e hora atuais
+now=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Gerar mensagem de commit
+commit_message="Commit realizado às $now"
+
+# Fazer o commit no arquivo README.md
+git add README.md
+git commit -m "$commit_message"
+
+# Empurrar as alterações para o repositório remoto
+git push
